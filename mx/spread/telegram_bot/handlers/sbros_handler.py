2f6fd33a8a696d1796e0660edef6a1c73cef93ab
Suer-Tech <102809506+suer-tech@users.noreply.github.com>
2024-01-16 19:59:41 +0500

    Update sbros_handler.py

--- mx/spread/telegram_bot/handlers/sbros_handler.py
+++ mx/spread/telegram_bot/handlers/sbros_handler.py
@@ -4,7 +4,7 @@ async def generate_sbros_handlers(dp, currency):
     sbros_keyboard_name = f'{curr_low}_sbros_keyboard'
     sbros_keyboard = locals()[sbros_keyboard_name]
 
-    @dp.message_handler(Text(equals="Сброс ТВХ USD"))
-    async def fix_usd_tvh(message: types.Message):
-        mess = 'Сбросить точку входа по USD?'
+    @dp.message_handler(Text(equals=f"Сброс ТВХ {currency}"))
+    async def fix_tvh(message: types.Message):
+        mess = f'Сбросить точку входа по {currency}?'
         await bot.send_message(message.chat.id, mess, reply_markup=sbros_keyboard)
