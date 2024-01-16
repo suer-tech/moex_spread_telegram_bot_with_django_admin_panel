34ae7a171e62d0def78e73ec3a489eee8153f7f2
Suer-Tech <102809506+suer-tech@users.noreply.github.com>
2024-01-16 20:01:36 +0500

    Update currencies_handlers.py

--- mx/spread/telegram_bot/currencies_handlers.py
+++ mx/spread/telegram_bot/currencies_handlers.py
@@ -1,7 +1,6 @@
-currencies = ["USD", "EUR", "СNY"]  # или любые другие валюты, которые вам нужны
-
-async def generate_currencies_handlers(dp):
+async def generate_currencies_handlers(dp, currencies):
     for currency in currencies:
+
         await generate_sbros_handlers(dp, currency)
         await generate_signal_firstspread_handlers(dp, currency)
         await generate_signal_firstspread_sbros_handlers(dp, currency)
@@ -13,39 +12,23 @@ async def generate_currencies_handlers(dp):
         await generate_yes_no_handlers(dp, currency)
 
 
+def generate_states_for_currencies(currencies):
+    class YourState(StatesGroup):
+        pass
 
+    for currency in currencies:
+        state_name = f"new_{currency.lower()}"
+        only_state_name = f"new_{currency.lower()}_only"
+        tvh_state_name = f"new_{currency.lower()}_tvh"
+        spread_first_state_name = f"new_{currency.lower()}_spread_first"
 
+        setattr(YourState, state_name, State())
+        setattr(YourState, only_state_name, State())
+        setattr(YourState, tvh_state_name, State())
+        setattr(YourState, spread_first_state_name, State())
 
+    return YourState
 
+# Пример использования:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+YourState = generate_states_for_currencies(currencies)
