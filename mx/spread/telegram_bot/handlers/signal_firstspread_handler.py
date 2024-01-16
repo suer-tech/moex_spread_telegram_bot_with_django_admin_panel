cb5f190039fad413094e2e626721ebb9cce1412f
Suer-Tech <102809506+suer-tech@users.noreply.github.com>
2024-01-16 20:00:31 +0500

    Update signal_firstspread_handler.py

--- mx/spread/telegram_bot/handlers/signal_firstspread_handler.py
+++ mx/spread/telegram_bot/handlers/signal_firstspread_handler.py
@@ -4,59 +4,60 @@ async def generate_signal_firstspread_handlers(dp, currency):
     signal_firstspread_keyboard_name = f'{curr_low}_signal_firstspread_keyboard'
     signal_firstspread_keyboard = locals()[signal_firstspread_keyboard_name]
 
-    @dp.message_handler(Text(equals="USD, п"))
+    @dp.message_handler(Text(equals=f"{currency}, п"))
     async def with_puree(message: types.Message):
         # Открываем клавиатуру с кнопками для управления сигналами
         await message.answer("Выберите действие:", reply_markup=signal_firstspread_keyboard)
 
-    @dp.message_handler(Text(equals="Текущий сигнал по USD, п"))
+    @dp.message_handler(Text(equals=f"Текущий сигнал по {currency}, п"))
     async def current_signal_usd(message: types.Message):
         # Считываем текущий сигнал из файла и отправляем его пользователю
         try:
-            usd_tvh_file = 'usd_firstspread_and_signal.txt'
+            tvh_file = f'{curr_low}_firstspread_and_signal.txt'
 
             if os.path.exists(usd_tvh_file):
-                with open(usd_tvh_file, "r") as file:
+                with open(tvh_file, "r") as file:
                     data = file.read().strip()  # Убираем лишние пробелы и переводы строк
 
                 if not data:
-                    await message.answer("Текущий сигнал по USD не установлен",
+                    await message.answer(f"Текущий сигнал по {currency} не установлен",
                                          reply_markup=signal_firstspread_keyboard)
                 else:
                     values = data.split(', ')
-                    signal = values[-1]
-                    if not signal:
-                        await message.answer("Текущий сигнал по USD не установлен",
+                    signals = values[-1]
+                    if not signals:
+                        await message.answer(f"Текущий сигнал по {currency} не установлен",
                                              reply_markup=signal_firstspread_keyboard)
                     else:
-                        await message.answer(f"Текущий сигнал по USD: {signal}",
+                        await message.answer(f"Текущий сигнал по {currency}: {signals}",
                                              reply_markup=signal_firstspread_keyboard)
         except FileNotFoundError:
             await message.answer("Сигнал не найден", reply_markup=signal_firstspread_keyboard)
 
-    @dp.message_handler(Text(equals="Новый сигнал по USD, п"))
+    @dp.message_handler(Text(equals=f"Новый сигнал по {currency}, п"))
     async def new_signal_usd(message: types.Message, state: FSMContext):
         # Запрашиваем новое значение сигнала и записываем его в файл
-        await message.answer("Введите новое значение сигнала по USD:")
+        await message.answer(f"Введите новое значение сигнала по {currency}:")
         # Устанавливаем состояние для ожидания нового значения сигнала
-        await YourState.new_usd_spread_first.set()
+        yourstate = f'YourState.new_{curr_low}_spread_first.set()'
+        await locals()[yourstate]
 
-    @dp.message_handler(state=YourState.new_usd_spread_first)
+    @dp.message_handler(state=locals()[f'YourState.new_{curr_low}_spread_first.set()'])
     async def process_new_signal(message: types.Message, state: FSMContext):
-        usd_tvh_file = 'usd_tvh.txt'
+        tvh_file = f'{curr_low}_tvh.txt'
 
-        if os.path.exists(usd_tvh_file):
+        if os.path.exists(tvh_file):
             # Проверьте, является ли файл пустым
-            is_empty = not bool(open(usd_tvh_file, 'r', encoding='utf-8').read())
+            is_empty = not bool(open(tvh_file, 'r', encoding='utf-8').read())
 
             if is_empty:
-                with open(usd_tvh_file, 'a', encoding='utf-8') as fw:
-                    with open('usd.txt', 'r', encoding='utf-8') as file:
+                with open(tvh_file, 'a', encoding='utf-8') as fw:
+                    with open(f'{curr_low}.txt', 'r', encoding='utf-8') as file:
                         lines = file.readlines()
 
                     x = None
                     for line in lines:
-                        if "Спред Si - USDRUBF:" in line:
+                        if f"Спред {currency}:" in line:
                             parts = line.split()
                             x_index = parts.index('Спред') + 4
                             x = parts[x_index]
@@ -74,21 +75,21 @@ async def generate_signal_firstspread_handlers(dp, currency):
                 return  # Выходим из обработчика
 
             # Сохраняем новый сигнал в файл
-            with open("usd_firstspread_and_signal.txt", "w") as file:
+            with open(f"{curr_low}_firstspread_and_signal.txt", "w") as file:
                 file.write(f"{x}, {data['new_signal']}")
-            await message.answer(f"{checkmark}Новый сигнал по USD установлен: {data['new_signal']}",
+            await message.answer(f"{checkmark}Новый сигнал по {currency} установлен: {data['new_signal']}",
                                  reply_markup=signal_firstspread_keyboard)
 
         # Завершаем состояние FSMContext
         await state.finish()
 
-    @dp.message_handler(Text(equals="Сбросить сигнал USD, п"))
+    @dp.message_handler(Text(equals=f"Сбросить сигнал {currency}, п"))
     async def reset_usd_tvh(message: types.Message):
-        usd_tvh_file = 'usd_firstspread_and_signal.txt'
+        tvh_file = f'{curr_low}_firstspread_and_signal.txt'
 
         if os.path.exists(usd_tvh_file):
-            with open(usd_tvh_file, 'w', encoding='utf-8') as fw:
+            with open(tvh_file, 'w', encoding='utf-8') as fw:
                 pass  # Это создаст пустой файл, стирая всё содержимое
-            await message.answer("Сигнал для USD сброшен.", reply_markup=signal_firstspread_keyboard)
+            await message.answer(f"Сигнал для {currency} сброшен.", reply_markup=signal_firstspread_keyboard)
         else:
             await message.answer("Нет сохранённого сигнала.", reply_markup=signal_firstspread_keyboard)
